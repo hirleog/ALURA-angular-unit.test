08f590e7d825940bce09c6b01a9cc4951b071eb8
hirleog <87393381+hirleog@users.noreply.github.com>
2023-01-13 17:27:25 -0300

    third unit teste lessons module

--- src/app/shared/components/like-widget/like-width.component.ts
+++ src/app/shared/components/like-widget/like-width.component.ts
@@ -1,13 +1,30 @@
-import { Component, EventEmitter, Output,Input } from '@angular/core';
-@Component ({
+import { UniqueServiceId } from './../../services/unique-id/unique-id.service';
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+@Component({
     selector: 'app-like-width',
     templateUrl: './like-width.component.html',
     styleUrls: ['like-width.component.scss']
 })
 
-export class LikeWidthComponent {
+export class LikeWidthComponent implements OnInit {
     @Output() public liked = new EventEmitter<void>();
     @Input() public likes = 0;
-    @Input() public id = 0;
-    
+    @Input() public id = null;
+    public fonts = { faThumbsUp }
+
+    constructor(private uniqueIdService: UniqueServiceId) {
+
+    }
+    ngOnInit(): void {
+        if (!this.id) {
+            this.id = this.uniqueIdService.generateUniqueIdWithPrefix('like')
+        }
+        // throw new Error('Method not implemented.');
+    }
+
+    public like(): void {
+        this.liked.emit()
+    }
+
 }
